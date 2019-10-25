ecd7c7a93a457251536c96dee2937d2c22ee274f
Tore Fadnes <tore.fadnes@ikamr.no>
2019-10-25 13:28:15 +0200

    First design ready. Started on drag-drop funk.

--- src/KDRS_Analyse/Form1.Designer.cs
+++ src/KDRS_Analyse/Form1.Designer.cs
@@ -28,13 +28,248 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rBtnInfoXml = new System.Windows.Forms.RadioButton();
+            this.rBtnDcmBlbRpt = new System.Windows.Forms.RadioButton();
+            this.rBtnDcmLog = new System.Windows.Forms.RadioButton();
+            this.rBtnDrdFiles = new System.Windows.Forms.RadioButton();
+            this.rBtnIKAVALog = new System.Windows.Forms.RadioButton();
+            this.rBtnIKAVAConvFiles = new System.Windows.Forms.RadioButton();
+            this.rBtnIKAVANoConvFiles = new System.Windows.Forms.RadioButton();
+            this.rBtnVera = new System.Windows.Forms.RadioButton();
+            this.rBtnArk5Xml = new System.Windows.Forms.RadioButton();
+            this.rBtnDcmN5val = new System.Windows.Forms.RadioButton();
+            this.rBtnProd = new System.Windows.Forms.RadioButton();
+            this.rBtnArch = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxInfoText = new System.Windows.Forms.TextBox();
+            this.txtBoxOutFolder = new System.Windows.Forms.TextBox();
+            this.txtBoxOutFile = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // rBtnInfoXml
+            // 
+            this.rBtnInfoXml.AutoSize = true;
+            this.rBtnInfoXml.Location = new System.Drawing.Point(12, 34);
+            this.rBtnInfoXml.Name = "rBtnInfoXml";
+            this.rBtnInfoXml.Size = new System.Drawing.Size(60, 17);
+            this.rBtnInfoXml.TabIndex = 0;
+            this.rBtnInfoXml.TabStop = true;
+            this.rBtnInfoXml.Text = "info.xml";
+            this.rBtnInfoXml.UseVisualStyleBackColor = true;
+            // 
+            // rBtnDcmBlbRpt
+            // 
+            this.rBtnDcmBlbRpt.AutoSize = true;
+            this.rBtnDcmBlbRpt.Location = new System.Drawing.Point(12, 57);
+            this.rBtnDcmBlbRpt.Name = "rBtnDcmBlbRpt";
+            this.rBtnDcmBlbRpt.Size = new System.Drawing.Size(152, 17);
+            this.rBtnDcmBlbRpt.TabIndex = 1;
+            this.rBtnDcmBlbRpt.TabStop = true;
+            this.rBtnDcmBlbRpt.Text = "Decom project blobs report";
+            this.rBtnDcmBlbRpt.UseVisualStyleBackColor = true;
+            // 
+            // rBtnDcmLog
+            // 
+            this.rBtnDcmLog.AutoSize = true;
+            this.rBtnDcmLog.Location = new System.Drawing.Point(12, 80);
+            this.rBtnDcmLog.Name = "rBtnDcmLog";
+            this.rBtnDcmLog.Size = new System.Drawing.Size(92, 17);
+            this.rBtnDcmLog.TabIndex = 2;
+            this.rBtnDcmLog.TabStop = true;
+            this.rBtnDcmLog.Text = "Decom full log";
+            this.rBtnDcmLog.UseVisualStyleBackColor = true;
+            // 
+            // rBtnDrdFiles
+            // 
+            this.rBtnDrdFiles.AutoSize = true;
+            this.rBtnDrdFiles.Location = new System.Drawing.Point(12, 103);
+            this.rBtnDrdFiles.Name = "rBtnDrdFiles";
+            this.rBtnDrdFiles.Size = new System.Drawing.Size(81, 17);
+            this.rBtnDrdFiles.TabIndex = 3;
+            this.rBtnDrdFiles.TabStop = true;
+            this.rBtnDrdFiles.Text = "DROID files";
+            this.rBtnDrdFiles.UseVisualStyleBackColor = true;
+            // 
+            // rBtnIKAVALog
+            // 
+            this.rBtnIKAVALog.AutoSize = true;
+            this.rBtnIKAVALog.Location = new System.Drawing.Point(12, 126);
+            this.rBtnIKAVALog.Name = "rBtnIKAVALog";
+            this.rBtnIKAVALog.Size = new System.Drawing.Size(73, 17);
+            this.rBtnIKAVALog.TabIndex = 4;
+            this.rBtnIKAVALog.TabStop = true;
+            this.rBtnIKAVALog.Text = "IKAVA log";
+            this.rBtnIKAVALog.UseVisualStyleBackColor = true;
+            // 
+            // rBtnIKAVAConvFiles
+            // 
+            this.rBtnIKAVAConvFiles.AutoSize = true;
+            this.rBtnIKAVAConvFiles.Location = new System.Drawing.Point(12, 149);
+            this.rBtnIKAVAConvFiles.Name = "rBtnIKAVAConvFiles";
+            this.rBtnIKAVAConvFiles.Size = new System.Drawing.Size(128, 17);
+            this.rBtnIKAVAConvFiles.TabIndex = 5;
+            this.rBtnIKAVAConvFiles.TabStop = true;
+            this.rBtnIKAVAConvFiles.Text = "IKAVA converted files";
+            this.rBtnIKAVAConvFiles.UseVisualStyleBackColor = true;
+            // 
+            // rBtnIKAVANoConvFiles
+            // 
+            this.rBtnIKAVANoConvFiles.AutoSize = true;
+            this.rBtnIKAVANoConvFiles.Location = new System.Drawing.Point(12, 172);
+            this.rBtnIKAVANoConvFiles.Name = "rBtnIKAVANoConvFiles";
+            this.rBtnIKAVANoConvFiles.Size = new System.Drawing.Size(146, 17);
+            this.rBtnIKAVANoConvFiles.TabIndex = 6;
+            this.rBtnIKAVANoConvFiles.TabStop = true;
+            this.rBtnIKAVANoConvFiles.Text = "IKAVA files not converted";
+            this.rBtnIKAVANoConvFiles.UseVisualStyleBackColor = true;
+            // 
+            // rBtnVera
+            // 
+            this.rBtnVera.AutoSize = true;
+            this.rBtnVera.Location = new System.Drawing.Point(12, 195);
+            this.rBtnVera.Name = "rBtnVera";
+            this.rBtnVera.Size = new System.Drawing.Size(67, 17);
+            this.rBtnVera.TabIndex = 7;
+            this.rBtnVera.TabStop = true;
+            this.rBtnVera.Text = "veraPDF";
+            this.rBtnVera.UseVisualStyleBackColor = true;
+            // 
+            // rBtnArk5Xml
+            // 
+            this.rBtnArk5Xml.AutoSize = true;
+            this.rBtnArk5Xml.Location = new System.Drawing.Point(12, 218);
+            this.rBtnArk5Xml.Name = "rBtnArk5Xml";
+            this.rBtnArk5Xml.Size = new System.Drawing.Size(86, 17);
+            this.rBtnArk5Xml.TabIndex = 8;
+            this.rBtnArk5Xml.TabStop = true;
+            this.rBtnArk5Xml.Text = "Arkade 5 xml";
+            this.rBtnArk5Xml.UseVisualStyleBackColor = true;
+            // 
+            // rBtnDcmN5val
+            // 
+            this.rBtnDcmN5val.AutoSize = true;
+            this.rBtnDcmN5val.Location = new System.Drawing.Point(12, 241);
+            this.rBtnDcmN5val.Name = "rBtnDcmN5val";
+            this.rBtnDcmN5val.Size = new System.Drawing.Size(142, 17);
+            this.rBtnDcmN5val.TabIndex = 9;
+            this.rBtnDcmN5val.TabStop = true;
+            this.rBtnDcmN5val.Text = "Documaster N5 validator";
+            this.rBtnDcmN5val.UseVisualStyleBackColor = true;
+            // 
+            // rBtnProd
+            // 
+            this.rBtnProd.AutoSize = true;
+            this.rBtnProd.Location = new System.Drawing.Point(12, 292);
+            this.rBtnProd.Name = "rBtnProd";
+            this.rBtnProd.Size = new System.Drawing.Size(76, 17);
+            this.rBtnProd.TabIndex = 10;
+            this.rBtnProd.TabStop = true;
+            this.rBtnProd.Text = "Production";
+            this.rBtnProd.UseVisualStyleBackColor = true;
+            // 
+            // rBtnArch
+            // 
+            this.rBtnArch.AutoSize = true;
+            this.rBtnArch.Location = new System.Drawing.Point(12, 315);
+            this.rBtnArch.Name = "rBtnArch";
+            this.rBtnArch.Size = new System.Drawing.Size(63, 17);
+            this.rBtnArch.TabIndex = 11;
+            this.rBtnArch.TabStop = true;
+            this.rBtnArch.Text = "Archival";
+            this.rBtnArch.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Input type";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 276);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "File type";
+            // 
+            // txtBoxInfoText
+            // 
+            this.txtBoxInfoText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBoxInfoText.Location = new System.Drawing.Point(304, 18);
+            this.txtBoxInfoText.Multiline = true;
+            this.txtBoxInfoText.Name = "txtBoxInfoText";
+            this.txtBoxInfoText.ReadOnly = true;
+            this.txtBoxInfoText.Size = new System.Drawing.Size(484, 136);
+            this.txtBoxInfoText.TabIndex = 16;
+            // 
+            // txtBoxOutFolder
+            // 
+            this.txtBoxOutFolder.Location = new System.Drawing.Point(15, 351);
+            this.txtBoxOutFolder.Name = "txtBoxOutFolder";
+            this.txtBoxOutFolder.Size = new System.Drawing.Size(443, 20);
+            this.txtBoxOutFolder.TabIndex = 17;
+            // 
+            // txtBoxOutFile
+            // 
+            this.txtBoxOutFile.Location = new System.Drawing.Point(15, 377);
+            this.txtBoxOutFile.Name = "txtBoxOutFile";
+            this.txtBoxOutFile.Size = new System.Drawing.Size(443, 20);
+            this.txtBoxOutFile.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBoxOutFile);
+            this.Controls.Add(this.txtBoxOutFolder);
+            this.Controls.Add(this.txtBoxInfoText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rBtnArch);
+            this.Controls.Add(this.rBtnProd);
+            this.Controls.Add(this.rBtnDcmN5val);
+            this.Controls.Add(this.rBtnArk5Xml);
+            this.Controls.Add(this.rBtnVera);
+            this.Controls.Add(this.rBtnIKAVANoConvFiles);
+            this.Controls.Add(this.rBtnIKAVAConvFiles);
+            this.Controls.Add(this.rBtnIKAVALog);
+            this.Controls.Add(this.rBtnDrdFiles);
+            this.Controls.Add(this.rBtnDcmLog);
+            this.Controls.Add(this.rBtnDcmBlbRpt);
+            this.Controls.Add(this.rBtnInfoXml);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton rBtnInfoXml;
+        private System.Windows.Forms.RadioButton rBtnDcmBlbRpt;
+        private System.Windows.Forms.RadioButton rBtnDcmLog;
+        private System.Windows.Forms.RadioButton rBtnDrdFiles;
+        private System.Windows.Forms.RadioButton rBtnIKAVALog;
+        private System.Windows.Forms.RadioButton rBtnIKAVAConvFiles;
+        private System.Windows.Forms.RadioButton rBtnIKAVANoConvFiles;
+        private System.Windows.Forms.RadioButton rBtnVera;
+        private System.Windows.Forms.RadioButton rBtnArk5Xml;
+        private System.Windows.Forms.RadioButton rBtnDcmN5val;
+        private System.Windows.Forms.RadioButton rBtnProd;
+        private System.Windows.Forms.RadioButton rBtnArch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxInfoText;
+        private System.Windows.Forms.TextBox txtBoxOutFolder;
+        private System.Windows.Forms.TextBox txtBoxOutFile;
     }
 }
 
--- src/KDRS_Analyse/Form1.cs
+++ src/KDRS_Analyse/Form1.cs
@@ -12,9 +12,34 @@ namespace KDRS_Analyse
 {
     public partial class Form1 : Form
     {
+
+        string fileName = string.Empty;
+        string outFolder = String.Empty;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            fileName
+            if (files.Count() > 1)
+                MessageBox.Show("One file at the time");
+            else
+            {
+
+            }
         }
     }
 }
