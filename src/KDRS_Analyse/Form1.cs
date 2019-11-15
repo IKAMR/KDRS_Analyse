4bea367b5b3c933e8128bd3df7c1a254879b19f8
Tore Fadnes <tore.fadnes@ikamr.no>
2019-11-15 14:42:40 +0100

    Updated version number.

--- src/KDRS_Analyse/Form1.cs
+++ src/KDRS_Analyse/Form1.cs
@@ -210,7 +210,7 @@ namespace KDRS_Analyse
     public static class Globals
     {
         public static readonly String toolName = "KDRS Analyse";
-        public static readonly String toolVersion = "0.1";
+        public static readonly String toolVersion = "0.2";
 
         public static int toolCounter = 0;
         public static ExtractionAnalyse extractionAnalyse = new ExtractionAnalyse();
